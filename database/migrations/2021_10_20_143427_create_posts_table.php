0857c8327196bacd87588a05f9658bd27392dcfb
yamamurodaisuke <daisuke0308@icloud.com>
2021-10-21 17:56:02 +0900

    postsテーブル作成

---
+++ database/migrations/2021_10_20_143427_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id'); 
+            $table->integer('control_number');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            //外部キーの設定
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
