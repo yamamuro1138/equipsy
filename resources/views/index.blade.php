0d03a566f6e7894d4e64d7a7e192d7be9aa2ead5
yamamurodaisuke <daisuke0308@icloud.com>
2021-10-21 18:08:49 +0900

    ナビバーに表示のユーザーネームのリンク削除

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,7 +41,7 @@
   <!-- Fixed navbar -->
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">{{ Auth::user()->name }}</a>
+      <a class="navbar-brand">{{ Auth::user()->name }}</a> 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
