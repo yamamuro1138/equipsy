b8747990ee71a9eb6030c405df5d8e7d202d289f
yamamurodaisuke <daisuke0308@icloud.com>
2021-11-02 15:41:24 +0900

    エラー分の修正

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -30,7 +30,7 @@ return [
         'array'   => ':attributeは、:min個から:max個の間で指定してください。',
     ],
     'boolean'              => ':attributeは、trueかfalseを指定してください。',
-    'confirmed'            => ':attributeと、確認フィールドとが、一致していません。',
+    'confirmed'            => ':attributeと、確認フィールドが、一致していません。',
     'date'                 => ':attributeには有効な日付を指定してください。',
     'date_equals'          => ':attributeには、:dateと同じ日付けを指定してください。',
     'date_format'          => ':attributeは:format形式で指定してください。',
