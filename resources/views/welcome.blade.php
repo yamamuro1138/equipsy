a25d29f43546ebf738c6d127d8191efe8c4a4c4e
yamamurodaisuke <daisuke0308@icloud.com>
2021-11-04 16:10:15 +0900

    不要ファイル削除及びインデントを整えた

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
-
 <!doctype html>
 <html lang="ja" class="h-100">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -9,58 +9,50 @@
     <meta name="generator" content="Hugo 0.79.0">
     <title>equipsy</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
 
-    
+
 
     <!-- Bootstrap core CSS -->
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-      }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
     </style>
 
-    
-  </head>
-  <body class="d-flex h-100 text-center text-black bg-light" >
-    
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <!-- <header class=""> -->
-    <div>
-      <!-- <h3 class="float-md-start mb-0">equipsy</h3> -->
-      <nav class="nav nav-masthead justify-content-center float-md-end">
-        <!-- <a class="nav-link" href="#">Contact</a> -->
-      </nav>
+</head>
+
+<body class="d-flex h-100 text-center text-black bg-light">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <!-- <header class=""> -->
+        <div>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+            </nav>
+        </div>
+        </header>
+
+        <main class="px-3">
+            <h1 class="display-2" style="line-height:3">equipsy</h1>
+            <p class="lead display-6">equipsyは、備品を管理するツールです。</p>
+            <p class="lead display-6">初めてご利用の方は、新規登録からどうぞ。</p>
+            <p class="lead" style="line-height:5">
+
+                <a href="{{ ('/register') }}" class="btn btn-lg btn-secondary ">新規登録</a>
+                <a href="{{ ('/login') }}" class="btn btn-lg btn-secondary ">ログイン</a>
+            </p>
+        </main>
     </div>
-  </header>
-
-  <main class="px-3">
-    <h1 class="display-2" style="line-height:3" >equipsy</h1>
-    <p class="lead display-6">equipsyは、備品を管理するツールです。</p>
-    <p class="lead display-6">初めてご利用の方は、新規登録からどうぞ。</p>
-    <p class="lead" style="line-height:5">
-
-    <a href="{{ ('/register') }}" class="btn btn-lg btn-secondary ">新規登録</a>
-    <a href="{{ ('/login') }}" class="btn btn-lg btn-secondary ">ログイン</a>
-  </p>
-  </main>
-
-</div>
-
-
-    
   </body>
 </html>
-
