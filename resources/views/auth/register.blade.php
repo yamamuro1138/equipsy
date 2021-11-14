29ffd9d6182b13b71331e786366787c4cc96ca01
yamamurodaisuke <daisuke0308@icloud.com>
2021-11-14 11:49:20 +0900

    httpsに変更

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="card-body">
                     <!-- <form method="POST" action="{{ route('register') }}"> -->
-                    <form action="http://equipsy.net/register" method="post" name="sample"> 
+                    <form action="https://equipsy.net/register" method="post" name="sample"> 
                         @csrf
 
                         <div class="form-group row">
