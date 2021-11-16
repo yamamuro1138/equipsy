fc9dbb22b15c6d434e7d39df4c19b887dc0aabe3
yamamurodaisuke <daisuke0308@icloud.com>
2021-11-17 00:01:15 +0900

    vueをコメントアウト

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+// window.Vue = require('vue').default;
 
 /**
  * The following block of code may be used to automatically register your
@@ -27,6 +27,6 @@ window.Vue = require('vue').default;
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+// const app = new Vue({
+//     el: '#app',
+// });
